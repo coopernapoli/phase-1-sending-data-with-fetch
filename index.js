70488694f8fad98677992e7c53d00f2c4572c667
Sean <napolisc10@gmail.com>
2023-01-03 10:57:59 -0700

    commit tree 6efbd496eff03ebc6467366bc28f57046bb8e5df

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ name: name, email: email })
+    })
+    .then(response => response.json())
+    .then(response => {
+      // Append the id from the response to the DOM
+      const newId = response.id;
+      document.body.appendChild(document.createTextNode(newId));
+    })
+    .catch(error => {
+        document.body.appendChild(document.createTextNode(error.message));
+      })
+    }
+  
+
+  
+  
+  
